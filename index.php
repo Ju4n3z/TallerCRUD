bfdcd8af915ddd87b9a899c44ead9cb05b9c945e
JuanEstebanGomezGarciaCAMPUS <juanesteban.gomezgarcia@gmail.com>
2023-06-07 17:27:22 -0500

    feat: :tada: Se creó el repositorio
    Se creó el repositorio con la plantilla del proyecto ya implementada

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container text-center mt-2">
+        <form>
+            <div class="row justify-content-center mt-2">
+                <div class="col-4">
+                    <input type="text" placeholder="Nombre">
+                </div>
+                <div class="col-4">
+                    CampusLands
+                </div>
+            </div>
+            <div class="row justify-content-center mt-2">
+                <div class="col-4">
+                    <input type="text" placeholder="Apellidos">
+                </div>
+                <div class="col-4">
+                    <input type="number" placeholder="Edad">
+                </div>
+            </div>
+            <div class="row justify-content-center mt-2">
+                <div class="col-4">
+                    <input type="text" placeholder="Dirección">
+                </div>
+                <div class="col-4">
+                    <input type="email" placeholder="Email">
+                </div>
+            </div>
+            <div class="row justify-content-center mt-2">
+                <div class="col-4">
+                    <input type="text" placeholder="Horario de entrada">
+                </div>
+                <div class="col-2">
+                    <input type="submit" value="✅">
+                </div>
+                <div class="col-2">
+                    <input type="submit" value="❌">
+                </div>
+            </div>
+            <div class="row justify-content-center mt-2">
+                <div class="col-4">
+                    <input type="text" placeholder="Team">
+                </div>
+                <div class="col-2">
+                    <input type="submit" value="🖍">
+                </div>
+                <div class="col-2">
+                    <input type="submit" value="🔍">
+                </div>
+            </div>
+            <div class="row justify-content-center mt-2">
+                <div class="col-4">
+                    <input type="text" placeholder="Trainer">
+                </div>
+                <div class="col-4">
+                    <input type="number" placeholder="Cédula">
+                </div>
+            </div>
+        </form>
+        <table class="table mt-3">
+            <thead>
+              <tr>
+                <th scope="col">Nombres</th>
+                <th scope="col">Apellidos</th>
+                <th scope="col">Direccion</th>
+                <th scope="col">Edad</th>
+                <th scope="col">Email</th>
+                <th scope="col">Hora Entrada</th>
+                <th scope="col">Team</th>
+                <th scope="col">Trainer</th>
+              </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
